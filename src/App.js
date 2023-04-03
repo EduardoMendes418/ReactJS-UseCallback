4e6335588566178b06d43798430e2bdf6ed1a242
Eduardo Mendes <eduardo.mendes@sensedia.com>
2023-04-02 21:21:44 -0300

    use-memo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import './App.css';
-import React, { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import P from 'prop-types';
-import { useCallback } from 'react';
+import './App.css';
 
-const Button = React.memo(function Button({ incrementButton }) {
+// New Component
+const Post = ({ post }) => {
    console.log('filho');
-   return <button onClick={() => incrementButton(100)}>+</button>;
-});
+   return (
+      <div key={post.id} className="post">
+         <h1>{post.title}</h1>
+         <p>{post.body}</p>
+      </div>
+   );
+};
 
-Button.protoTypes = {
-   incrementButton: P.func,
+//Tipagem
+Post.prototype = {
+   post: P.shape({
+      id: P.number,
+      title: P.string,
+      body: P.string,
+   }),
 };
 
 function App() {
-   const [counter, setCounter] = useState(0);
-
-   const incrementCounter = useCallback((num) => {
-      setCounter((c) => c + num);
-   }, []);
-
+   const [posts, setPosts] = useState([]);
+   const [value, setValue] = useState('');
    console.log('pai');
+
+   //component did mount
+   useEffect(() => {
+      setTimeout(() => {
+         fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((r) => r.json())
+            .then((r) => setPosts(r));
+      }, 2000);
+   }, []);
 
    return (
       <div className="App">
-         <p>Teste 3</p>
-         <h1>C1:{counter}</h1>
-         <Button incrementButton={incrementCounter} />
+         <p>
+            <input
+               type="search"
+               value={value}
+               onChange={(e) => setValue(e.target.value)}
+            />
+         </p>
+         {useMemo(() => {
+            return (
+               posts.length > 0 &&
+               posts.map((post) => {
+                  return <Post key={post.id} post={post} />;
+               })
+            );
+         }, [posts])}
+         {posts.length <= 0 && <p>Ainda nao tem post</p>}
       </div>
    );
 }
-
 export default App;
